45fd3620fb66b38a9e5937d8b1024db3ef0c390a
t-bast <bastuc@hotmail.fr>
2018-09-27 17:35:04 +0200

    Set 3: Challenge 3

--- challenge/set3_test.go
+++ challenge/set3_test.go
@@ -1,13 +1,20 @@
 package challenge
 
 import (
+	"bufio"
+	"crypto/rand"
 	"encoding/base64"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/t-bast/cryptopals/cipher/stream"
 	"github.com/t-bast/cryptopals/oracle"
+	"github.com/t-bast/cryptopals/xor"
 )
 
 func TestSet3_Challenge1(t *testing.T) {
@@ -25,4 +32,38 @@ func TestSet3_Challenge2(t *testing.T) {
 	decrypted := ctr.Decrypt(ciphertext)
 
 	assert.Equal(t, "Yo, VIP Let's kick it Ice, Ice, baby Ice, Ice, baby ", string(decrypted))
+}
+
+func TestSet3_Challenge3(t *testing.T) {
+	testData := filepath.Join("testdata", "3_3.txt")
+	f, err := os.OpenFile(testData, os.O_RDONLY, os.ModePerm)
+	require.NoError(t, err)
+	defer f.Close()
+
+	key := make([]byte, 16)
+	rand.Read(key)
+	ctr := stream.NewCTR(key, 0)
+
+	reader := bufio.NewReader(f)
+	var encrypted [][]byte
+	for {
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+
+		lineBytes, err := base64.StdEncoding.DecodeString(string(line))
+		require.NoError(t, err)
+
+		encrypted = append(encrypted, ctr.Encrypt(lineBytes))
+	}
+
+	keystream := stream.PwnCTRNonceReuse(encrypted)
+	decrypted := xor.Bytes(encrypted[0], keystream[:len(encrypted[0])])
+
+	// The current method does a single pass on letter frequency.
+	// It works well for the beginning of the message but can miss the end.
+	// However it's then easy to test several candidate keystream bytes for the
+	// end of the message manually until it makes sense.
+	assert.Equal(t, 0, strings.Index(string(decrypted), "I have met them at close of"))
 }
--- cipher/stream/ctr.go
+++ cipher/stream/ctr.go
@@ -4,6 +4,7 @@ import (
 	"crypto/aes"
 	"encoding/binary"
 
+	"github.com/t-bast/cryptopals/score"
 	"github.com/t-bast/cryptopals/xor"
 )
 
@@ -52,4 +53,47 @@ func (e *CTR) Encrypt(message []byte) []byte {
 // Decrypt a message in CTR mode.
 func (e *CTR) Decrypt(ciphertext []byte) []byte {
 	return e.Encrypt(ciphertext)
+}
+
+// PwnCTRNonceReuse takes multiples ciphertexts generated with the same nonce and
+// figures out the keystream.
+func PwnCTRNonceReuse(ciphertexts [][]byte) []byte {
+	maxLength := 0
+	for _, ciphertext := range ciphertexts {
+		if len(ciphertext) > maxLength {
+			maxLength = len(ciphertext)
+		}
+	}
+
+	var keystream []byte
+	for i := 0; i < maxLength; i++ {
+		bestCandidate := byte(0)
+		bestScore := float32(0)
+
+		for j := 0; j < 256; j++ {
+			currentScore := float32(0.0)
+			for _, ciphertext := range ciphertexts {
+				if len(ciphertext) < i+1 {
+					continue
+				}
+
+				plain := ciphertext[i] ^ byte(j)
+				if plain < 32 || 122 < plain {
+					currentScore = 0
+					break
+				} else {
+					currentScore += score.LetterFrequency([]byte{plain})
+				}
+			}
+
+			if currentScore > bestScore {
+				bestCandidate = byte(j)
+				bestScore = currentScore
+			}
+		}
+
+		keystream = append(keystream, bestCandidate)
+	}
+
+	return keystream
 }
